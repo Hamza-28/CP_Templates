935b94885008629079b1a4a6643d4d283fb8580c
Md. Saimum Islam Hamza <129812963+Hamza-28@users.noreply.github.com>
2025-02-20 14:51:58 +0600

    Update Geometric Sum.cpp

--- code/Code/Geometric Sum.cpp
+++ code/Code/Geometric Sum.cpp
@@ -7,3 +7,11 @@ ll geometric_Sum() {
 	ll ans = (numara * BigMod(deno, MOD - 2)) % MOD;
 	return ans;
 }
+// geometric sum for any MOD
+const int mod = 1e9 + 7;
+	int geo_all(int n, int x) { //1 + x + x^2 + x^3 ....x^n = f(n, x)
+	if (n == 0) return 0;
+	int ret = 1ll * (1 + x) * geo_all(n / 2, 1ll * x * x % mod) % mod;
+	if (n & 1) ret = (1 + 1ll * x * ret) % mod;
+	return ret;
+}

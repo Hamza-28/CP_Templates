2f2a8628c162158648b40ff4c27afcf7ce65f171
Md. Saimum Islam Hamza <129812963+Hamza-28@users.noreply.github.com>
2025-02-20 15:32:29 +0600

    Update ComputeSignedArea.cpp

--- code/Geometry/ComputeSignedArea.cpp
+++ code/Geometry/ComputeSignedArea.cpp
@@ -13,3 +13,18 @@ double ComputeSignedArea(const vector<PT> &p) {
 double ComputeArea(const vector<PT> &p) {
   return fabs(ComputeSignedArea(p));
 }
+// integer area
+void computeIntArea() {
+	int n; cin >> n;
+	point arr[n];
+	for (int i = 0; i < n; i++) {
+		arr[i].read();
+	}
+	point a = {0, 0};
+	ll ans = 0;
+	for (int i = 0; i + 1 < n; i++) {
+		ans += a.triangle(arr[i], arr[i + 1]);
+	}
+	ans += a.triangle(arr[n - 1],	arr[0]);
+	cout << abs(ans) << "\n";
+}

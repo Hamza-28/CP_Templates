b975cf722067fcc7aa64179c283e84b0491131e4
Md. Saimum Islam Hamza <129812963+Hamza-28@users.noreply.github.com>
2025-02-20 14:52:44 +0600

    Update Geometric Sum.cpp

--- code/Code/Geometric Sum.cpp
+++ code/Code/Geometric Sum.cpp
@@ -9,7 +9,7 @@ ll geometric_Sum() {
 }
 // geometric sum for any MOD
 const int mod = 1e9 + 7;
-	int geo_all(int n, int x) { //1 + x + x^2 + x^3 ....x^n = f(n, x)
+int geo_all(int n, int x) { //1 + x + x^2 + x^3 ....x^n = f(n, x)
 	if (n == 0) return 0;
 	int ret = 1ll * (1 + x) * geo_all(n / 2, 1ll * x * x % mod) % mod;
 	if (n & 1) ret = (1 + 1ll * x * ret) % mod;

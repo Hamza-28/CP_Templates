3c11392b2899aa89673159f8aa53ac847646230a
Md. Saimum Islam Hamza <129812963+Hamza-28@users.noreply.github.com>
2025-02-20 15:16:42 +0600

    Update Custom Hash.cpp

--- code/Code/Custom Hash.cpp
+++ code/Code/Custom Hash.cpp
@@ -11,3 +11,19 @@ struct custom_hash {
  	}
 };
 unordered_map<long long int, int, custom_hash> mp; // this will work when the key is an int or long long int
+
+// hash pair
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+ 
+        if (hash1 != hash2) {
+            return hash1 ^ hash2;              
+        }
+        // If hash1 == hash2, their XOR is zero.
+          return hash1;
+    }
+};
